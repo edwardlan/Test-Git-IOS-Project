c66dfd7b2e46e618236fb2aeb14d72e5eedc0bdc
Edward Lan <lan@Edwards-Mac-mini.local>
2014-06-06 15:46:44 -0700

    added a test class

---
+++ Git Test/ELTest.h
@@ -0,0 +1,13 @@
+//
+//  ELTest.h
+//  Git Test
+//
+//  Created by Edward Lan on 6/6/14.
+//  Copyright (c) 2014 IST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ELTest : NSObject
+
+@end
---
+++ Git Test/ELTest.m
@@ -0,0 +1,13 @@
+//
+//  ELTest.m
+//  Git Test
+//
+//  Created by Edward Lan on 6/6/14.
+//  Copyright (c) 2014 IST. All rights reserved.
+//
+
+#import "ELTest.h"
+
+@implementation ELTest
+
+@end
